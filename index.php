6ac7a6f1fcd7273e6b12868bf5e90c7e1fac77ac
EasyDev <easy-dev@web.de>
2017-11-28 08:46:15 +0100

    Finished feature/DashboardFooterLink

--- index.php
+++ index.php
@@ -133,10 +133,13 @@
 							<p class="text-h3"><a href="?site=dashboard"><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</a></p>
 						</div>
 						<div class="col-2">
-							<p class="text-h3"><a href="?site=dashboard"><i class="fa fa-tachometer" aria-hidden="true"></i> Add Post</a></p>
+							<?php echo "<p class=\"text-h3\"><a href=\"?site=dashboard&siteid=".$iSite."&action=add_post\"><i class=\"fa fa-plus-circle\" aria-hidden=\"true\"></i> Add Page</a></p>"; ?>
 						</div>
 						<div class="col-2">
-							<p class="text-h3"><a href="?site=dashboard"><i class="fa fa-tachometer" aria-hidden="true"></i> Add Page</a></p>
+							<?php echo "<p class=\"text-h3\"><a href=\"?site=dashboard&siteid=".$iSite."&action=add_page\"><i class=\"fa fa-plus-circle\" aria-hidden=\"true\"></i> Add Page</a></p>"; ?>
+						</div>
+						<div class="col-2">
+							<?php echo "<p class=\"text-h3\"><a href=\"?site=dashboard&siteid=".$iSite."&action=view_pages\"><i class=\"fa fa-columns\" aria-hidden=\"true\"></i> View Pages</a></p>"; ?>
 						</div>
 						<div class="col-2">
 							<?php echo "<p class=\"text-h3\"><a href=\"?site=dashboard&siteid=".$iSite."&action=settings\"><i class=\"fa fa-cog\" aria-hidden=\"true\"></i> Settings</a></p>"; ?>
