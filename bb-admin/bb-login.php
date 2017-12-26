9b87b861d5de0af83720fc2dedee374e7c0a43f4
EasyDev <easy-dev@web.de>
2017-12-26 23:23:49 +0100

    Added: User Edit function

--- bb-admin/bb-login.php
+++ bb-admin/bb-login.php
@@ -1,23 +1,23 @@
 <?php
 	/*
-			MaterialBlocks - Froala design-blocks based CMS
-			Copyright (C) 2017  Robin Krause
-	
-			This program is free software: you can redistribute it and/or modify
-			it under the terms of the GNU General Public License as published by
-			the Free Software Foundation, either version 3 of the License, or
-			(at your option) any later version.
-	
-			This program is distributed in the hope that it will be useful,
-			but WITHOUT ANY WARRANTY; without even the implied warranty of
-			MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-			GNU General Public License for more details.
-	
-			You should have received a copy of the GNU General Public License
-			along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		*/
+		MaterialBlocks - Froala design-blocks based CMS
+		Copyright (C) 2017  Robin Krause
+
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	*/
 		
-		function LoginForm() {
+	function LoginForm() {
 	?>
 	<section class="fdb-block fdb-viewport" style="background-color: #242424; color: #EEE;">
     	<div class="container justify-content-center align-items-center d-flex">
